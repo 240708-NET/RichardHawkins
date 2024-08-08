85c14e225429e777ef51c6666e2212993a45b5a6
Richard Hawkins <Richard.Hawkins@Revature.com>
2024-08-08 14:24:02 -0500

    add React tutorials

---
+++ React/Next-tutorial/.next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"mtmj5N8q/Ylr1msqWA7tTAmVbGc6ts/BvfcUGC7Itxg=\"\n}"
---
+++ React/Next-tutorial/.next/static/webpack/app/page.d0becdad86ff1384.hot-update.js
@@ -0,0 +1,21 @@
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+self["webpackHotUpdate_N_E"]("app/page",{
+
+/***/ "(app-pages-browser)/./node_modules/next/dist/build/webpack/loaders/next-flight-client-entry-loader.js?modules=%7B%22request%22%3A%22%2Frun%2Fmedia%2Fhawkins%2FStorage-1%2FRevature%2F1979-240708-dotnet%2FRichardHawkins%2FReact%2FNext-tutorial%2Fapp%2Flike-button.js%22%2C%22ids%22%3A%5B%22default%22%5D%7D&server=false!":
+/*!******************************************************************************************************************************************************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/next/dist/build/webpack/loaders/next-flight-client-entry-loader.js?modules=%7B%22request%22%3A%22%2Frun%2Fmedia%2Fhawkins%2FStorage-1%2FRevature%2F1979-240708-dotnet%2FRichardHawkins%2FReact%2FNext-tutorial%2Fapp%2Flike-button.js%22%2C%22ids%22%3A%5B%22default%22%5D%7D&server=false! ***!
+  \******************************************************************************************************************************************************************************************************************************************************************************************************************/
+/***/ (function(__unused_webpack_module, __unused_webpack_exports, __webpack_require__) {
+
+eval(__webpack_require__.ts("Promise.resolve(/*! import() eager */).then(__webpack_require__.bind(__webpack_require__, /*! ./app/like-button.js */ \"(app-pages-browser)/./app/like-button.js\"));\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiKGFwcC1wYWdlcy1icm93c2VyKS8uL25vZGVfbW9kdWxlcy9uZXh0L2Rpc3QvYnVpbGQvd2VicGFjay9sb2FkZXJzL25leHQtZmxpZ2h0LWNsaWVudC1lbnRyeS1sb2FkZXIuanM/bW9kdWxlcz0lN0IlMjJyZXF1ZXN0JTIyJTNBJTIyJTJGcnVuJTJGbWVkaWElMkZoYXdraW5zJTJGU3RvcmFnZS0xJTJGUmV2YXR1cmUlMkYxOTc5LTI0MDcwOC1kb3RuZXQlMkZSaWNoYXJkSGF3a2lucyUyRlJlYWN0JTJGTmV4dC10dXRvcmlhbCUyRmFwcCUyRmxpa2UtYnV0dG9uLmpzJTIyJTJDJTIyaWRzJTIyJTNBJTVCJTIyZGVmYXVsdCUyMiU1RCU3RCZzZXJ2ZXI9ZmFsc2UhIiwibWFwcGluZ3MiOiJBQUFBLGtLQUFnTCIsInNvdXJjZXMiOlsid2VicGFjazovL19OX0UvP2M4YTEiXSwic291cmNlc0NvbnRlbnQiOlsiaW1wb3J0KC8qIHdlYnBhY2tNb2RlOiBcImVhZ2VyXCIsIHdlYnBhY2tFeHBvcnRzOiBbXCJkZWZhdWx0XCJdICovIFwiL3J1bi9tZWRpYS9oYXdraW5zL1N0b3JhZ2UtMS9SZXZhdHVyZS8xOTc5LTI0MDcwOC1kb3RuZXQvUmljaGFyZEhhd2tpbnMvUmVhY3QvTmV4dC10dXRvcmlhbC9hcHAvbGlrZS1idXR0b24uanNcIik7XG4iXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=\n//# sourceURL=webpack-internal:///(app-pages-browser)/./node_modules/next/dist/build/webpack/loaders/next-flight-client-entry-loader.js?modules=%7B%22request%22%3A%22%2Frun%2Fmedia%2Fhawkins%2FStorage-1%2FRevature%2F1979-240708-dotnet%2FRichardHawkins%2FReact%2FNext-tutorial%2Fapp%2Flike-button.js%22%2C%22ids%22%3A%5B%22default%22%5D%7D&server=false!\n"));
+
+/***/ })
+
+});
